5419172fde9cf20e58592335cf35117b7840a7d1
Victor Torre <web.ehooo@gmail.com>
2015-11-30 00:00:33 +0100

    dhcp init

---
+++ red_casa/dhcp/management/commands/dhcp_server.py
@@ -0,0 +1,165 @@
+from __future__ import absolute_import
+
+from red_casa.dhcp.models import *
+
+from django.utils import six
+from django.conf import settings
+from django.utils.encoding import get_system_encoding, force_text
+from django.core.management.base import BaseCommand, CommandError
+
+from scapy.all import *
+from datetime import datetime
+import threading
+import socket
+import errno
+import sys
+
+
+def response(pkg, mac, ip, stdout=None, stderr=None):
+    if stdout:
+        stdout.write("%s\n" % pkg.summary())
+    dhcp = pkg.getlayer(DHCP)
+    if dhcp:
+        recv_ops = dict(filter(lambda x: type(x) == tuple, dhcp.options))
+        message_type = recv_ops.get('message-type', None)
+
+        if message_type == 1:
+            client_ip, dhcp_options = get_dhcp_options(pkg.src, 'offer', ip)
+            if client_ip is None:
+                if stderr:
+                    stderr.write("There is not more IPs.\n")
+                return
+            if stdout:
+                stdout.write('New DHCP discovered %(mac)s\n' % {'mac': pkg.src})
+        elif message_type == 3:
+            recv_ip_cliente = recv_ops.get('requested_addr', None)
+            if recv_ip_cliente is None:
+                if stderr:
+                    stderr.write("There is not Requested Address.\n")
+                return
+            client_ip, dhcp_options = get_dhcp_options(pkg.src, 'ack', ip)
+            if client_ip != recv_ip_cliente:
+                dhcp_options = [('message-type', 'nak'), ('server_id', ip), 'end']
+                if stderr:
+                    stderr.write('DHCP Ack NOT match %(ip_db)s %(ip_recv)s\n' % {'ip_db': client_ip,
+                                                                                 'ip_recv': recv_ip_cliente})
+            if stdout:
+                stdout.write('New DHCP request %(mac)s\n' % {'mac': pkg.src})
+        else:
+            return
+        dhcp_pkg = Ether(src=mac, dst=pkg.src)/\
+                   IP(src=ip, dst=client_ip)/\
+                   UDP(sport=67, dport=68)/\
+                   BOOTP(op=2, yiaddr=client_ip, siaddr=ip, giaddr='0.0.0.0', xid=pkg[BOOTP].xid)/\
+                   DHCP(options=dhcp_options)
+        sendp(dhcp_pkg)
+        if stdout:
+            stdout.write('Send DHCP pkg: %s\n' % dhcp_pkg.summary())
+
+
+def get_dhcp_options(dhcp_type, mac, ip):
+    ops = []
+    try:
+        user = DHCPUser.objects.get(address=mac)
+    except DHCPUser.DoesNotExist:
+        user = DHCPUser.objects.create(address=mac)
+        # TODO Buscar directamente una IP en la red por defecto
+    if user.has_ip_available():
+        ops = user.gen_options(dhcp_type, server_id=ip)
+    else:
+        if user.search_ip():
+            ops = user.gen_options(dhcp_type, server_id=ip)
+        else:
+            # TODO ver si se no ha asignado por no tener historial previo
+            # o si hay que buscar una red por defecto para generar las IPs de esa red
+            return None, ops
+    return user.ip.address, ops
+
+
+class Command(BaseCommand):
+    help = 'Create a debug dhcp server'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--iface', action='store', dest='iface', default=None, help='Interface for DHCP server.')
+        parser.add_argument('--nothreading', action='store_false', dest='use_threading', default=True,
+                            help='Tells Django to NOT use threading.')
+        parser.add_argument('--mac', action='store', dest='mac', default=None, help='Mac of DHCP server.')
+        parser.add_argument('--ip', action='store', dest='ip', default=None, help='DHCP server ip.')
+        parser.add_argument('--remote', action='store', dest='remote', default='github.com',
+                            help='IP or host to connect for detect the own IP.')
+
+    def execute(self, *args, **options):
+        if options.get('no_color'):
+            # We rely on the environment because it's currently the only
+            # way to reach WSGIRequestHandler. This seems an acceptable
+            # compromise considering `runserver` runs indefinitely.
+            os.environ[str("DJANGO_COLORS")] = str("nocolor")
+        super(Command, self).execute(*args, **options)
+
+    def handle(self, *args, **options):
+        shutdown_message = options.get('shutdown_message', '')
+        quit_command = 'CTRL-BREAK' if sys.platform == 'win32' else 'CONTROL-C'
+        self.stdout.write("Performing system checks...\n\n")
+        self.check(display_num_errors=True)
+        now = datetime.now().strftime('%B %d, %Y - %X')
+        if six.PY2:
+            now = now.decode(get_system_encoding())
+        self.stdout.write(now)
+        self.stdout.write((
+            "Django version %(version)s, using settings %(settings)r\n"
+            "Starting development server\n"
+            "Quit the server with %(quit_command)s.\n"
+        ) % {
+            "version": self.get_version(),
+            "settings": settings.SETTINGS_MODULE,
+            "quit_command": quit_command,
+        })
+
+        try:
+            self.server(**options)
+        except socket.error as e:
+            # Use helpful error messages instead of ugly tracebacks.
+            ERRORS = {
+                errno.EACCES: "You don't have permission to access that port.",
+                errno.EADDRINUSE: "That port is already in use.",
+                errno.EADDRNOTAVAIL: "That IP address can't be assigned to.",
+            }
+            try:
+                error_text = ERRORS[e.errno]
+            except KeyError:
+                error_text = force_text(e)
+            self.stderr.write("Error: %s" % error_text)
+            # Need to use an OS exit because sys.exit doesn't work in a thread
+            os._exit(1)
+        except KeyboardInterrupt:
+            if shutdown_message:
+                self.stdout.write(shutdown_message)
+            sys.exit(0)
+
+    def server(self, **options):
+        iface = options.get('iface', None)
+        fam, hw = get_if_raw_hwaddr(iface)
+        mac = options.get('mac', hw)
+        ip = options.get('ip', None)
+        if ip is None:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.connect((options.get('remote', 'github.com'), 80))
+            ip, port = s.getsockname()
+            s.close()
+
+        def prn(pkg):
+            if options.get('use_threading', True):
+                threading.Thread(target=response, args=(pkg, mac, ip, self.stdout, self.stderr)).start()
+            else:
+                response(pkg, mac, ip, self.stdout, self.stderr)
+
+        def lfilter(pkg):
+            if pkg.haslayer(DHCP):
+                dhcp = pkg.getlayer(DHCP)
+                if dhcp and pkg.sport == 68 and pkg.dport == 67:
+                    recv_ops = dict(filter(lambda x: type(x) == tuple, dhcp.options))
+                    message_type = recv_ops.get('message-type', None)
+                    if (message_type == 1 and pkg.dst == 'ff:ff:ff:ff:ff:ff') or message_type == 3:
+                        return True
+            return False
+        sniff(filter='port 67 or port 68', lfilter=lfilter, prn=prn, iface=iface)
---
+++ red_casa/dhcp/models.py
@@ -0,0 +1,417 @@
+from django.conf import settings
+from django.db import models
+import ipaddr
+from datetime import datetime, timedelta
+import socket
+
+DHCP_LEASE_DEFAULT = 43200
+if hasattr(settings, 'DHCP_LEASE_DEFAULT'):
+    DHCP_LEASE_DEFAULT = settings.DHCP_LEASE_DEFAULT
+DHCP_IP_LIST_SEPARATOR = ','
+if hasattr(settings, 'DHCP_IP_LIST_SEPARATOR'):
+    DHCP_IP_LIST_SEPARATOR = settings.DHCP_IP_LIST_SEPARATOR
+DHCP_IP_DISCOVER_HOST = 'github.com'
+if hasattr(settings, 'DHCP_IP_DISCOVER_HOST'):
+    DHCP_IP_DISCOVER_HOST = settings.DHCP_IP_DISCOVER_HOST
+
+SUBNET_MASK = 1
+TIME_ZONE = 2
+ROUTER = 3
+TIME_SERVER = 4
+NAME_SERVER = 6
+LOG_SERVER = 7
+COOKIE_SERVER = 8
+HOSTNAME = 12
+DOMAIN = 15
+BROADCAST_ADDRESS = 28
+NTP_SERVER = 42
+NETBIOS_SERVER = 44
+NETBIOS_DIST_SERVER = 45
+REQUESTED_ADDR = 50
+LEASE_TIME = 51
+SERVER_ID = 54
+CLIENT_ID = 61
+SMTP_SERVER = 69
+POP3_SERVER = 70
+NNTP_SERVER = 71
+WWW_SERVER = 72
+
+DHCP_OPTIONS = [
+    (SUBNET_MASK, "subnet_mask"),  # Solo valido para usuario
+    (TIME_ZONE, "time_zone"),
+    (ROUTER, "router"),  # Solo valido para usuario
+    (TIME_SERVER, "time_server"),
+    (NAME_SERVER, "name_server"),  # Solo valido para usuario
+    (LOG_SERVER, "log_server"),
+    (COOKIE_SERVER, "cookie_server"),
+    (HOSTNAME, "hostname"),
+    (DOMAIN, "domain"),  # Solo valido para usuario
+    (BROADCAST_ADDRESS, "broadcast_address"),
+    (NTP_SERVER, "NTP_server"),
+    (NETBIOS_SERVER, "NetBIOS_server"),
+    (NETBIOS_DIST_SERVER, "NetBIOS_dist_server"),
+    # (REQUESTED_ADDR, "requested_addr"),
+    (LEASE_TIME, "lease_time"),
+    (SERVER_ID, "server_id"),
+    (CLIENT_ID, "client_id"),
+    (SMTP_SERVER, "SMTP_server"),
+    (POP3_SERVER, "POP3_server"),
+    (NNTP_SERVER, "NNTP_server"),
+    (WWW_SERVER, "WWW_server")
+]
+
+
+class DHCPOption(models.Model):
+    option = models.IntegerField(choices=DHCP_OPTIONS)
+    value = models.TextField()
+
+    def __unicode__(self):
+        return "(%s, %s)" % (self.option, self.value)
+
+    def __str__(self):
+        return "(%s, %s)" % (self.option, self.value)
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        if self.option in [HOSTNAME, DOMAIN]:  # Valid string
+            str(self.value)
+        elif self.option in [SUBNET_MASK, BROADCAST_ADDRESS, REQUESTED_ADDR, SERVER_ID]:
+            ipaddr.IPAddress(self.value)
+        elif self.option in [ROUTER, TIME_SERVER, NAME_SERVER, LOG_SERVER, COOKIE_SERVER, NTP_SERVER,
+                             NETBIOS_SERVER, NETBIOS_DIST_SERVER, SMTP_SERVER, POP3_SERVER, NNTP_SERVER, WWW_SERVER]:
+            for ip in self.value.split(DHCP_IP_LIST_SEPARATOR):
+                ipaddr.IPAddress(ip)
+        elif self.option == TIME_ZONE:  # Valid signed int
+            int(self.value)
+        elif self.option == LEASE_TIME:  # Valid unigned int
+            if int(self.value) < 0:
+                raise ValueError('value must be unsigned int')
+        super(models.Model, self).save(force_insert, force_update, using, update_fields)
+
+    def get_value(self):
+        if self.option in [ROUTER, TIME_SERVER, NAME_SERVER, LOG_SERVER, COOKIE_SERVER, NTP_SERVER,
+                           NETBIOS_SERVER, NETBIOS_DIST_SERVER, SMTP_SERVER, POP3_SERVER, NNTP_SERVER, WWW_SERVER]:
+            items = []
+            for ip in self.value.split(DHCP_IP_LIST_SEPARATOR):
+                items.append(ip)
+            return items
+        elif self.option in [TIME_ZONE, LEASE_TIME]:
+            return int(self.value)
+        return self.value
+
+
+class DHCPNetwork(models.Model):
+    name = models.CharField(max_length=32, blank=True, null=True, help_text='For helping to understand')
+    router = models.CharField(max_length=20)
+    subnet_mask = models.CharField(max_length=20)
+    name_server = models.CharField(max_length=255)
+    domain = models.CharField(max_length=255, default='local')
+    options = models.ManyToManyField(DHCPOption)
+
+    class Meta:
+        unique_together = ('router', 'subnet_mask')
+
+    def __unicode__(self):
+        return "%s/%s" % (self.router, self.subnet_mask)
+
+    def __str__(self):
+        return "%s/%s" % (self.router, self.subnet_mask)
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        nets = []
+        for router in self.router.split(DHCP_IP_LIST_SEPARATOR):
+            ipaddr.IPAddress(router)
+            nets.append(ipaddr.IPNetwork("%s/%s" % (router, self.subnet_mask)))
+        if len(set(nets)) != 1:
+            raise ValueError('Only one network is allowed')
+        for ip in self.name_server.split(DHCP_IP_LIST_SEPARATOR):
+            ipaddr.IPAddress(ip)
+        super(models.Model, self).save(force_insert, force_update, using, update_fields)
+
+
+class DHCPip(models.Model):
+    network = models.ForeignKey(DHCPNetwork)
+    address = models.CharField(max_length=20)
+
+    class Meta:
+        unique_together = ('network', 'address')
+        ordering = ['address']
+
+    def __unicode__(self):
+        return self.address
+
+    def __str__(self):
+        return self.address
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        ip = ipaddr.IPAddress(self.address)
+        one_router = None
+        for router in self.network.router.split(DHCP_IP_LIST_SEPARATOR):
+            one_router = router
+            if ip == ipaddr.IPAddress(router):
+                raise ValueError('Router IP is not allowed')
+        if ip not in ipaddr.IPNetwork("%s/%s" % (one_router, self.network.subnet_mask)):
+            raise ValueError('IP is outside the network')
+        super(models.Model, self).save(force_insert, force_update, using, update_fields)
+
+    def get_network(self):
+        router = self.ip.network.router.split(DHCP_IP_LIST_SEPARATOR)[0]
+        return ipaddr.IPNetwork("%s/%s" % (router, self.ip.network.subnet_mask))
+
+
+class DHCPUser(models.Model):
+    name = models.CharField(max_length=32, blank=True, null=True, help_text='For helping to understand')
+    address = models.CharField(max_length=20, primary_key=True)
+    static = models.BooleanField(default=False)
+    ip = models.ForeignKey(DHCPip, null=True, blank=True)
+    options = models.ManyToManyField(DHCPOption)
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        if self.static:
+            if not self.ip:
+                raise ValueError('For static IP must be assigned')
+        if self.ip:
+            subnet_mask = None
+            try:
+                subnet_mask = self.options.get(option=SUBNET_MASK).get_value()
+            except DHCPOption.DoesNotExist:
+                pass
+            try:
+                routers = self.options.get(option=ROUTER).get_value()
+                if not subnet_mask:
+                    subnet_mask = self.ip.network.subnet_mask
+                ip = ipaddr.IPAddress(self.ip.address)
+                inside_net = False
+                for router in routers:
+                    if ip == ipaddr.IPAddress(router):
+                        raise ValueError('Router IP is not allowed')
+                    if ip in ipaddr.IPNetwork("%s/%s" % (router, subnet_mask)):
+                        inside_net = True
+                if not inside_net:
+                    raise ValueError('IP is outside the network')
+            except DHCPOption.DoesNotExist:
+                if subnet_mask:
+                    routers = self.network.router.split(DHCP_IP_LIST_SEPARATOR)
+                    ip = ipaddr.IPAddress(self.ip.address)
+                    inside_net = False
+                    for router in routers:
+                        if ip in ipaddr.IPNetwork("%s/%s" % (router, subnet_mask)):
+                            inside_net = True
+                            break
+                    if not inside_net:
+                        raise ValueError('IP is outside the network')
+        super(models.Model, self).save(force_insert, force_update, using, update_fields)
+
+    @property
+    def subnet_mask(self):
+        try:
+            return self.options.get(option=SUBNET_MASK).get_value()
+        except DHCPOption.DoesNotExist:
+            if self.ip:
+                return self.ip.network.subnet_mask
+
+    @property
+    def lease_time(self):
+        try:
+            return self.options.get(option=LEASE_TIME).get_value()
+        except DHCPOption.DoesNotExist:
+            try:
+                return self.ip.network.options.get(option=LEASE_TIME).get_value()
+            except DHCPOption.DoesNotExist:
+                pass
+        return DHCP_LEASE_DEFAULT
+
+    @property
+    def router(self):
+        try:
+            return self.options.get(option=ROUTER).get_value()
+        except DHCPOption.DoesNotExist:
+            if self.ip:
+                return self.ip.network.router.split(DHCP_IP_LIST_SEPARATOR)
+
+    @property
+    def domain(self):
+        try:
+            return self.options.get(option=DOMAIN).get_value()
+        except DHCPOption.DoesNotExist:
+            if self.ip:
+                return self.ip.network.domain
+
+    @property
+    def name_server(self):
+        try:
+            return self.options.get(option=NAME_SERVER).get_value()
+        except DHCPOption.DoesNotExist:
+            if self.ip:
+                return self.ip.network.name_server.split(DHCP_IP_LIST_SEPARATOR)
+
+    def get_network(self):
+        subnet_mask = self.subnet_mask
+        if not subnet_mask:
+            return None
+        return ipaddr.IPNetwork("%s/%s" % (self.ip.address, subnet_mask))
+
+    @property
+    def history_ips(self):
+        return DHCPHistory.objects.filter(mac=self).order_by('-date').distinct('ip')
+
+    def has_ip_available(self):
+        if not self.ip:
+            return False
+        if self.static:
+            return True
+        old_user = DHCPHistory.who_is(self.ip)
+        if old_user == self:
+            return True
+        return DHCPHistory.has_available(self.ip)
+
+    def gen_options(self, dhcp_type, server_id=None, host_discovery=None):
+        ops = [('message-type', dhcp_type)]
+        if not self.ip:
+            raise RuntimeError('Ip is not assigned yet')
+        ops.append(('subnet_mask', self.subnet_mask))
+
+        if server_id:
+            ops.append(('server_id', server_id))
+        else:
+            try:
+                ops.append(('server_id', self.options.get(option=SERVER_ID).get_value()))
+            except DHCPOption.DoesNotExist:
+                try:
+                    ops.append(('server_id', self.ip.network.options.get(option=SERVER_ID).get_value()))
+                except DHCPOption.DoesNotExist:
+                    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                    if host_discovery:
+                        s.connect((host_discovery, 80))
+                    else:
+                        s.connect((DHCP_IP_DISCOVER_HOST, 80))
+                    self_ip, port = s.getsockname()
+                    s.close()
+                    ops.append(('server_id', self_ip))
+
+        ops.append(('lease_time', self.lease_time))
+        ops.append(('router', self.router))
+        ops.append(('domain', self.domain))
+        ops.append(('name_server', self.name_server))
+
+        for (num, key) in DHCP_OPTIONS:
+            if num in [SUBNET_MASK, SERVER_ID, LEASE_TIME, ROUTER, DOMAIN, NAME_SERVER]:
+                continue
+            try:
+                ops.append((key, self.options.get(option=num).get_value()))
+            except DHCPOption.DoesNotExist:
+                try:
+                    ops.append((key, self.ip.network.options.get(option=num).get_value()))
+                except DHCPOption.DoesNotExist:
+                    continue
+        ops.append('end')
+        return ops
+
+    def search_ip(self):
+        found = False
+        for candidate in self.history_ips:
+            ip_candidate = ipaddr.IPAddress(candidate.ip.address)
+            routers = []
+            try:
+                routers = self.options.get(option=ROUTER).get_value()
+            except DHCPOption.DoesNotExist:
+                pass
+            routers.extend(candidate.ip.network.router.split(DHCP_IP_LIST_SEPARATOR))
+            candidate.ip.get_network()
+            nets = []
+            for ip in routers:
+                net = ipaddr.IPNetwork("%s/%s" % (ip, self.subnet_mask))
+                if net in nets:
+                    continue
+                nets.append(net)
+                if ip_candidate in net and DHCPHistory.has_available(candidate.ip):
+                    found = True
+                    break
+
+            if not found:
+                if len(nets) > 1:
+                    min_net = nets[0]
+                    for net in set(nets):
+                        if net < min_net:
+                            min_net = net
+                    first_candidate = None
+                    for ip in min_net:
+                        if ip in routers:
+                            continue
+                        try:
+                            next_ip = DHCPip.objects.get(address=str(ip), network=candidate.ip.network)
+                            if first_candidate is None and DHCPHistory.has_available(next_ip):
+                                first_candidate = next_ip
+                        except DHCPip.DoesNotExist:
+                            self.ip = DHCPip.objects.create(address=str(ip), network=candidate.ip.network)
+                            found = True
+                            break
+                    if found:
+                        break
+                    elif first_candidate is not None:
+                        self.ip = first_candidate
+                        found = True
+                        break
+                else:
+                    continue
+            else:
+                self.ip = candidate.ip
+                break
+
+        return found
+
+
+
+
+class DHCPHistory(models.Model):
+    ip = models.ForeignKey(DHCPip)
+    mac = models.ForeignKey(DHCPUser)
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('mac', 'ip')
+        ordering = ['date']
+
+    @staticmethod
+    def has_available(ip):
+        """
+        :param ip: Ip asking for
+        :type ip: DHCPip
+        :return: If this IP ar available for assigned to new user
+        """
+        user = DHCPHistory.who_is(ip)
+        if user and user.ip != ip:  # La ip ya no esta asignada a este usuario
+                return True
+        else:
+            old_user = DHCPHistory.who_is(ip, True)
+            if not old_user or not old_user.static:
+                return True
+        return False
+
+    @staticmethod
+    def who_is(ip, last=False):
+        """
+        :param ip: Ip asking for
+        :type ip: DHCPip
+        :param last: If ip has expired, return last user assigned
+        :type last: Boolean
+        :return: DHCPUser or None
+        """
+        history = DHCPHistory.objects.filter(ip=ip)
+        last_assign = history.last()
+        if not last_assign:
+            return None
+        old_user = last_assign.mac
+        lease = DHCP_LEASE_DEFAULT
+        try:
+            lease = old_user.options.get(option=LEASE_TIME)
+        except DHCPOption.DoesNotExist:
+            try:
+                lease = ip.network.options.get(option=LEASE_TIME)
+            except DHCPOption.DoesNotExist:
+                pass
+        expire = last_assign.date + timedelta(seconds=lease)
+        if expire < datetime.utcnow():
+            if last:
+                return old_user
+            return None  # La asignacion ha cadudado se le puede volver a asignar
+        return old_user
